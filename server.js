ca097af8906f0b6329c4d2c46c5c2a6cef86dc27
Naiem Al Foysal <naiemalfoysal@Naiems-MacBook-Pro.local>
2022-06-25 22:42:10 +0600

    vercel5

--- server.js
+++ server.js
@@ -1,59 +1,59 @@
-const express = require("express");
-const morgan = require("morgan");
-const connectDB = require("./config/db");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-
-// Config dotev
-// require("dotenv").config({
-//   path: "./config/config.env",
-// });
-require("dotenv").config({
-  path: ".env",
-});
-
-const app = express();
-app.use(cors());
-
-// Connect to database
-connectDB();
-
-// body parser
-app.use(bodyParser.json());
-// Load routes
-const authRouter = require("./routes/auth.route");
-const userRouter = require("./routes/user.route");
-const eventRouter = require("./routes/events.route");
-
-// Dev Logginf Middleware
-app.use(morgan("dev"));
-
-// Use Routes
-app.use("/api", authRouter);
-app.use("/api", userRouter);
-app.use("/api", eventRouter);
-
-app.use((req, res) => {
-  res.status(404).json({
-    success: false,
-    msg: "Page not founded",
-  });
-});
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-});
-
 // const express = require("express");
+// const morgan = require("morgan");
+// const connectDB = require("./config/db");
+// const bodyParser = require("body-parser");
+// const cors = require("cors");
+
+// // Config dotev
+// // require("dotenv").config({
+// //   path: "./config/config.env",
+// // });
+// require("dotenv").config({
+//   path: ".env",
+// });
+
 // const app = express();
-// var cors = require("cors");
 // app.use(cors());
-// app.get("/api/", function (req, res) {
-//   res.json({ msg: "Naiem Al Foysal" });
+
+// // Connect to database
+// connectDB();
+
+// // body parser
+// app.use(bodyParser.json());
+// // Load routes
+// const authRouter = require("./routes/auth.route");
+// const userRouter = require("./routes/user.route");
+// const eventRouter = require("./routes/events.route");
+
+// // Dev Logginf Middleware
+// app.use(morgan("dev"));
+
+// // Use Routes
+// app.use("/api", authRouter);
+// app.use("/api", userRouter);
+// app.use("/api", eventRouter);
+
+// app.use((req, res) => {
+//   res.status(404).json({
+//     success: false,
+//     msg: "Page not founded",
+//   });
 // });
-// const PORT = 5000;
+
+// const PORT = process.env.PORT || 5000;
+
 // app.listen(PORT, () => {
-//   console.log(`Your app running successfully on port:${PORT}`);
+//   console.log(`App listening on port ${PORT}`);
 // });
+
+const express = require("express");
+const app = express();
+var cors = require("cors");
+app.use(cors());
+app.get("/", function (req, res) {
+  res.json({ msg: "Naiem Al Foysal" });
+});
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Your app running successfully on port:${PORT}`);
+});
